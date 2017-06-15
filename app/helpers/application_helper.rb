e66af62b284b0f7fe05a1c5210d97c4c0eb84ed5
Chris Flan <flanthedev@gmail.com>
2017-06-15 11:43:42 +1000

    add full_title method to application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # page title if provided or not
+  def full_title(page_title = '')
+    base_title = "BLLTN"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+  
 end
