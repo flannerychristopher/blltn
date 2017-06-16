592aaaf99f61184061248029676c636b5b65cf4d
Chris Flan <flanthedev@gmail.com>
2017-06-16 13:00:49 +1000

    complete basic user model and pw digest bcrypt

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -21,12 +21,6 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-  test "password should be present" do
-    @user = User.new(name: "John", email: "john@thebeatles.com",
-                     password: "", password_confirmation: "")
-    assert_not @user.valid?
-  end
-
   test "name should not be too long" do
     @user.name = "a" * 51
     assert_not @user.valid?
@@ -69,10 +63,14 @@ class UserTest < ActiveSupport::TestCase
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
+  test "password should be present" do
+    @user.password = @user.password_confirmation = "      "
+    assert_not @user.valid?
+  end
 
-
-
-
-
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "abcde"
+    assert_not @user.valid?
+  end
 
 end
