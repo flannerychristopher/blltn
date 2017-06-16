e05d87d65814a92642c1295f2b4538e9d52dfc63
Chris Flan <flanthedev@gmail.com>
2017-06-16 19:35:46 +1000

    refactor stylesheet, layout

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,13 +3,16 @@ require 'test_helper'
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
   test "layout links" do
+    # home page
     get root_path
     assert_template 'static_pages/home'
     assert_select   "a[href=?]",    root_path, count: 2
     assert_select   "a[href=?]",    help_path
+    # help page
     get help_path
     assert_select   "title",        full_title("HELP")
     assert_select   "div#accordion"
+    # join page
     get join_path
     assert_template 'users/new'
     assert_select   "title",        full_title("JOIN")
