f860d2a8e5043074c47a221e420a7807041016ed
Chris Flan <flanthedev@gmail.com>
2017-06-16 10:34:07 +1000

    refactor link styling

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,14 +5,14 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select   "a[href=?]",  root_path, count: 2
-    assert_select   "a[href=?]",  help_path
+    assert_select   "a[href=?]",    root_path, count: 2
+    assert_select   "a[href=?]",    help_path
     get help_path
-    assert_select   "title",      full_title("HELP")
+    assert_select   "title",        full_title("HELP")
     assert_select   "div#accordion"
     get signup_path
     assert_template 'users/new'
-    assert_select   "title",      full_title("SIGN UP")
+    assert_select   "title",        full_title("SIGN UP")
   end
 
 end
