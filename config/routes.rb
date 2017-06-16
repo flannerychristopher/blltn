0d8fde03225484d1830486cc381df71c890549d8
Chris Flan <flanthedev@gmail.com>
2017-06-16 20:53:45 +1000

    progress on log in page and error flash

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
 
   root                      'static_pages#home'
   get     '/help',      to: 'static_pages#help'
+
   get     '/join',      to: 'users#new'
   post    '/join',      to: 'users#create'
+
+  get     '/login',     to: 'sessions#new'
+  post    '/login',     to: 'sessions#create'
+  delete  '/logout',    to: 'sessions#destroy'
 
   resources :users
 
