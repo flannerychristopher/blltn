2a90c14e84386f69ce871135f6c447c25ad35bee
Chris Flan <flanthedev@gmail.com>
2017-06-18 18:39:48 +1000

    finish basic user edit

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,12 +5,19 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # remembers a user in a persistent session
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  # returns true if the given user is the current user
+  def current_user?(user)
+    user == current_user
+  end
+
+  # returns the user corresponding to the remember token cookie
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: session[:user_id])
@@ -38,6 +45,16 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  # redirects to stored location (or to default)
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 
 end
---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,46 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:john)
+  end
+
+  test "unsuccessful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { name:         "",
+                                              email:        "foo@invalid",
+                                              bio:          "",
+                                              password:              "foo",
+                                              password_confirmation: "bar" } }
+    assert_template 'users/edit'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+  end
+
+  test "successful edit with friendly forwarding" do
+    # visit edit page not logged in, then log in
+    get edit_user_path(@user)
+    assert_not_nil session[:forwarding_url]
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    bio = "singer in a band"
+    patch user_path(@user), params: { user: { name:   name,
+                                              email:  email,
+                                              bio:    bio,
+                                              password:              "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    assert_nil session[:forwarding_url]
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+    assert_equal bio, @user.bio
+  end
+
+end
