ff7c4327c4c5eebc914ab4fa0676c2c77a794cc7
Chris Flan <flanthedev@gmail.com>
2017-07-11 12:48:46 +1000

    add more membership controller tests, reorganize other tests

--- test/controllers/boards_controller_test.rb
+++ test/controllers/boards_controller_test.rb
@@ -1,7 +1,72 @@
 require 'test_helper'
 
 class BoardsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @john = users(:john)
+    @ringo = users(:ringo)
+    @thebeatles = boards(:thebeatles)
+  end
+
+  test "can view boards without logging in" do
+      get boards_path
+      assert_template 'boards/index'
+      get board_path(@thebeatles)
+      assert_template 'boards/show'
+      assert_select 'title',    full_title(@thebeatles.name.upcase)
+  end
+
+  test "must log in to edit board" do
+    get edit_board_path(@thebeatles)
+    follow_redirect!
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+  end
+
+  test "user can create a board" do
+    log_in_as(@ringo)
+    get new_board_path
+    assert_difference 'Board.count' do
+      post boards_path, params: { board: { name: "The Allstars",
+                                           info: "checkout my new band" } }
+    end
+    follow_redirect!
+    assert_not flash.empty?
+    assert_template 'boards/show'
+  end
+
+  test "should redirect edit when logged in as non admin" do
+    log_in_as(@ringo)
+    get edit_board_path(@thebeatles), params: { id: @thebeatles.id }
+    assert_redirected_to root_url
+  end
+
+  test "admin view edit board" do
+    log_in_as(@john)
+    get edit_board_path(@thebeatles), params: { id: @thebeatles.id }
+  end
+
+  test "only admin can edit board" do
+    # log in non admin
+    log_in_as(@ringo)
+    get board_path(@thebeatles)
+    get edit_board_path(@thebeatles)
+    follow_redirect!
+    assert_template 'static_pages/home'
+    delete logout_path
+    # log in as admin
+    log_in_as(@john)
+    get board_path(@thebeatles)
+    get edit_board_path(@thebeatles)
+    assert_template 'boards/edit'
+    newinfo = "we just broke up."
+    patch board_path(@thebeatles), params: { board: { name: "The Beatles",
+                                                 info: newinfo } }
+    assert_redirected_to @thebeatles
+    assert_not flash.empty?
+    @thebeatles.reload
+    assert_equal newinfo, @thebeatles.info
+  end
+
+
 end
--- test/controllers/memberships_controller_test.rb
+++ test/controllers/memberships_controller_test.rb
@@ -5,12 +5,20 @@ class MembershipsControllerTest < ActionDispatch::IntegrationTest
   def setup
     @john = users(:john)
     @paul = users(:paul)
+    @ringo = users(:ringo)
     @thebeatles = boards(:thebeatles)
     @johnmembership = memberships(:john)
+    @ringomembership = memberships(:ringo)
   end
 
   test "can not change admin status through the web" do
-
+    log_in_as(@paul)
+    assert_no_difference 'Membership.where(admin: 1).count' do
+      post memberships_path, params: {
+                             membership: { user_id: @paul.id,
+                                           board_id: @thebeatles.id,
+                                           admin: 1 } }
+    end
   end
 
   test "should redirect create when not logged in" do
@@ -34,6 +42,19 @@ class MembershipsControllerTest < ActionDispatch::IntegrationTest
       delete membership_path(@johnmembership), params: { id: @johnmembership }
     end
     assert_redirected_to root_url
+  end
+
+  test "user can join boards" do
+    log_in_as(@paul)
+    assert_not @paul.memberships.where(board_id: @thebeatles.id).any?
+    # join
+    get new_membership_path
+    assert_difference '@paul.memberships.count', 1 do
+      post memberships_path, params:     {
+                             membership: { user_id: @paul.id,
+                                          board_id: @thebeatles.id } }
+    end
+    assert @paul.memberships.where(board_id: @thebeatles.id).any?
   end
 
 end
