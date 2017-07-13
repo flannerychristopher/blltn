eb3d23c4f06e3334e42eff4c5a0d0f62e2bc02aa
Chris Flan <flanthedev@gmail.com>
2017-07-13 19:39:59 +1000

    create and delete posts with ajax

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :logged_in_user,  only: [:create, :destroy]
+  before_action :correct_user,    only: :destroy
 
   def create
     @post = @current_user.posts.build(post_params)
@@ -10,7 +11,7 @@ class PostsController < ApplicationController
       respond_to do |format|
         format.html { redirect_to @board }
         format.js
-      end      
+      end
     else
       flash[:danger] = "post not created"
       redirect_to @board
@@ -19,12 +20,23 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
+
   end
 
   private
 
     def post_params
       params.require(:post).permit(:content, :board_id)
+    end
+
+    def correct_user
+      @post = @current_user.posts.find_by(id: params[:id])
+      redirect_to root_url if @post.nil?
     end
 
 end
